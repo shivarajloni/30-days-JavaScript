22a34a4dca6933873db0046c44cea6b4d9647d2a
shivaraj loni <shivarajbloni@gmail.com>
2020-09-13 22:09:25 +0530

    added day 3

---
+++ Day 3/variables.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    // console.log(this.dataset);
+    // console.log(suffix);
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mouseremove', handleUpdate));
+
